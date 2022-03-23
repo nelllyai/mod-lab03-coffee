f7d097aca1e760ac826efda430e8e3b1f26eaca4
Nelly Nikitina <100719863+nelllyai@users.noreply.github.com>
2022-03-24 01:03:39 +0300

    Update Automata.h

--- include/Automata.h
+++ include/Automata.h
@@ -1,5 +1,6 @@
 // Copyright 2022 UNN-IASR
 
+#pragma once
 #include <iostream>
 #include <string>
 
@@ -10,31 +11,31 @@ enum states {
 };
 
 class Automata {
-    private:
-        int cash;
-        string menu[5] = {
-            "Americano",
-            "Cappuccino",
-            "Black tea",
-            "Green tea",
-            "Hot chocolate"
-        };
-        int prices[5] = {
-            120, 150, 80, 100, 100
-        };
-        states state;
-        int selection;
+ private:
+    int cash;
+    string menu[5] = {
+        "Americano",
+        "Cappuccino",
+        "Black tea",
+        "Green tea",
+        "Hot chocolate"
+    };
+    int prices[5] = {
+        120, 150, 80, 100, 100
+    };
+    states state;
+    int selection;
 
-    public:
-        Automata();
-        void on();
-        void off();
-        void coin(int c);
-        void getMenu();
-        states getState();
-        void choice(int k);
-        bool check();
-        int cancel();
-        void cook();
-        int finish();
+ public:
+    Automata();
+    void on();
+    void off();
+    void coin(int c);
+    void getMenu();
+    states getState();
+    void choice(int k);
+    bool check();
+    int cancel();
+    void cook();
+    int finish();
 };
